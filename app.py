06b1883ec61a7c4f3dbb82e1d6f60a0829a04d9e
Jackson-509 <info.aristil.jackson@gmail.com>
2025-06-08 03:51:12 +0200

    Update app.py

--- app.py
+++ app.py
@@ -23,6 +23,7 @@ def index():
 
         flash("🎉 Réservation enregistrée avec succès !", "success")
         return redirect(url_for('index'))
-
-    return render_template('index.html')
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
