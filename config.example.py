b0fc53b34d74cf08655cf5844ee4b6513fe0a188
Jackson-509 <info.aristil.jackson@gmail.com>
2025-06-08 02:33:42 +0200

    Add files via upload

---
+++ config.example.py
@@ -0,0 +1,13 @@
+import os
+
+SECRET_KEY = 'votre_clef_secrete'
+
+MAIL_SETTINGS = {
+    'MAIL_SERVER': 'smtp.gmail.com',
+    'MAIL_PORT': 587,
+    'MAIL_USE_TLS': True,
+    'MAIL_USERNAME': 'votre.email@gmail.com',
+    'MAIL_PASSWORD': 'votre-mot-de-passe'
+}
+
+CSV_FILE = 'reservations.csv'
