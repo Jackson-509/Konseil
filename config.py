eeceb4ceca3c9923f08938842ef3dd66292081cb
Jackson-509 <info.aristil.jackson@gmail.com>
2025-06-09 22:03:32 +0200

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,11 @@ MAIL_SETTINGS = {
     'MAIL_PORT': int(os.getenv('MAIL_PORT', 587)),
     'MAIL_USE_TLS': os.getenv('MAIL_USE_TLS', 'True') == 'True',
     'MAIL_USERNAME': os.getenv('MAIL_USERNAME'),
-    'MAIL_PASSWORD': os.getenv('MAIL_PASSWORD')
+    'MAIL_PASSWORD': os.getenv('MAIL_PASSWORD'),
+    'MAIL_DEFAULT_SENDER': (
+        os.getenv('MAIL_SENDER_NAME', 'Cabinet Konseil'),
+        os.getenv('MAIL_USERNAME')
+    )
 }
 
 # 📄 Fichier CSV utilisé pour sauvegarde locale
